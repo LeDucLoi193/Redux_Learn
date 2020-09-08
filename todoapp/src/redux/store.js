7871dca1152ad8613ace5b7961825dac64ed3bf6
leducloi <dekaito193@gmail.com>
2020-09-08 22:56:09 +0700

    test redux middleware

--- todoapp/src/redux/store.js
+++ todoapp/src/redux/store.js
@@ -1,8 +1,26 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import todoReducer from './todo';
 
 const reducer = combineReducers({
   todo: todoReducer
 });
 
-export default createStore(reducer);
+// function myMiddleware(store) {
+//   return function(next) {
+//     return function(action) {
+
+//     }
+//   }
+// }
+
+const myMiddleware = store => next => action => {
+  if (action.type === "ADD_TODO" && action.payload === "dit") {
+    action.payload = "***"
+  }
+  return next(action);
+}
+
+export default createStore(
+  reducer,
+  applyMiddleware(myMiddleware)
+);
